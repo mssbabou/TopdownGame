7cf6ac789d11aeb1ea3147d7123582d6367c69e2
Markus <mssbabou@gmail.com>
2025-03-19 12:22:52 +0100

    Shoot

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -5,14 +5,36 @@ public class Bullet : MonoBehaviour
     float Damage;
     float Speed;
 
+
     void Update()
     {
-        transform.position += Speed * Time.deltaTime * transform.up;  
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, RotationToDirection2D(transform.rotation.z), Speed * Time.deltaTime);
+        if (hit)
+        {
+            Health health = hit.collider.gameObject.GetComponent<Health>();
+            if (health)
+            {
+                health.TakeDamage(Damage);
+            }
+        }
+        else
+        {
+            transform.position += Speed * Time.deltaTime * transform.up;  
+        }
     }
 
     public void Init(float damage, float speed)
     {
         Damage = damage;
         Speed = speed;
+    }
+
+    Vector2 RotationToDirection2D(float angleDegrees)
+    {
+        // Convert the angle to radians and then get the unit vector
+        float radians = angleDegrees * Mathf.Deg2Rad;
+        Vector2 direction = new Vector2(Mathf.Cos(radians), Mathf.Sin(radians));
+
+        return direction;
     }
 }

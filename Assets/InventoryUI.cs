afa66abede359b3affee28e488cae1fe0a55a66d
Markus <mssbabou@gmail.com>
2025-03-26 14:18:23 +0100

    UI Prefab

--- Assets/InventoryUI.cs
+++ Assets/InventoryUI.cs
@@ -8,8 +8,15 @@ public class InventoryUI : MonoBehaviour
     public GameObject GreenKeycardUI;
     public GameObject BlueKeycardUI;
 
+    private void Start()
+    {
+        PA = FindAnyObjectByType<PlayerAction>();
+    }
+
     private void Update()
     {
+        if (PA == null) return;
+
         RedKeycardUI.SetActive(PA.Inventory.Contains(ItemType.RedKeycard));
         GreenKeycardUI.SetActive(PA.Inventory.Contains(ItemType.GreenKeycard));
         BlueKeycardUI.SetActive(PA.Inventory.Contains(ItemType.BlueKeycard));

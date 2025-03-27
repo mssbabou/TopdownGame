c576d60bdb2ca733a8e07eaa01e76aa66b6fda40
Markus <mssbabou@gmail.com>
2025-03-27 11:55:38 +0100

    Add Vertical Doors

---
+++ Assets/EnemySpawner.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject EnemyPrefab;
+    public int Amount = 10;
+    public Vector2 SpawnAreaSize = Vector2.one;
+
+    public void Spawn()
+    {
+
+    }
+}
